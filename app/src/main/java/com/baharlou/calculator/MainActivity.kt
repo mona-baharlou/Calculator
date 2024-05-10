9d8ad7ec9a63d7601224cea620c646559308fff1
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-10 17:09:48 +0330

    code changed

--- app/src/main/java/com/baharlou/calculator/MainActivity.kt
+++ app/src/main/java/com/baharlou/calculator/MainActivity.kt
@@ -2,10 +2,108 @@ package com.baharlou.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.baharlou.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onNumberClicked()
+        onOperatorClicked()
     }
+
+
+    private fun onNumberClicked() {
+
+        binding.btnZero.setOnClickListener() {
+            appendText("0")
+        }
+
+        binding.btnOne.setOnClickListener() {
+            appendText("1")
+        }
+
+        binding.btnTwo.setOnClickListener() {
+            appendText("2")
+        }
+
+        binding.btnThree.setOnClickListener() {
+            appendText("3")
+        }
+
+        binding.btnFour.setOnClickListener() {
+            appendText("4")
+        }
+
+        binding.btnFive.setOnClickListener() {
+            appendText("5")
+        }
+
+        binding.btnSix.setOnClickListener() {
+            appendText("6")
+        }
+
+        binding.btnSeven.setOnClickListener() {
+            appendText("7")
+        }
+
+        binding.btnEight.setOnClickListener() {
+            appendText("8")
+        }
+
+        binding.btnNine.setOnClickListener() {
+            appendText("9")
+        }
+
+        binding.btnDot.setOnClickListener() {
+            appendText(".")
+        }
+    }
+
+    private fun appendText(text: String) {
+
+    }
+
+    private fun onOperatorClicked() {
+
+        binding.btnJam.setOnClickListener() {
+            appendText("+")
+        }
+
+        binding.btnMenha.setOnClickListener() {
+            appendText("-")
+        }
+
+        binding.btnZarb.setOnClickListener() {
+            appendText("*")
+        }
+
+        binding.btnTaqsim.setOnClickListener() {
+            appendText("/")
+        }
+
+        binding.btnParantezBaz.setOnClickListener() {
+            appendText("(")
+        }
+
+        binding.btnParantezBaste.setOnClickListener() {
+            appendText(")")
+        }
+
+        binding.btnAc.setOnClickListener() {
+            binding.txtExpression.text = ""
+            binding.txtResult.text = ""
+        }
+
+        binding.btnDelete.setOnClickListener {
+
+        }
+    }
+
+
 }
