073612fdff528d2d4cacb83a195b12097ae019b7
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-10 18:10:42 +0330

    code changed

--- app/src/main/java/com/baharlou/calculator/MainActivity.kt
+++ app/src/main/java/com/baharlou/calculator/MainActivity.kt
@@ -2,11 +2,16 @@ package com.baharlou.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewTreeObserver
+import android.widget.Toast
 import com.baharlou.calculator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private var operator: Char = '+'
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,7 +26,9 @@ class MainActivity : AppCompatActivity() {
     private fun onNumberClicked() {
 
         binding.btnZero.setOnClickListener() {
-            appendText("0")
+
+            if (binding.txtExpression.text.isNotEmpty())
+                appendText("0")
         }
 
         binding.btnOne.setOnClickListener() {
@@ -61,37 +68,114 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnDot.setOnClickListener() {
-            appendText(".")
+
+            if (binding.txtExpression.text.isEmpty()) {
+                appendText("0.")
+            } else if (!binding.txtExpression.text.contains("."))
+                appendText(".")
         }
     }
 
-    private fun appendText(text: String) {
+    private fun appendText(newText: String) {
+
+        if (binding.txtResult.text.isNotEmpty()) {
+            binding.txtExpression.text = ""
+        }
+
+        binding.txtResult.text = ""
+
+        binding.txtExpression.append(newText)
+
+
+        val viewTree: ViewTreeObserver = binding.horizontalScrollViewTxtExpression.viewTreeObserver
+        viewTree.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                binding.horizontalScrollViewTxtExpression.viewTreeObserver.removeOnGlobalLayoutListener(
+                    this
+                )
+
+                binding.horizontalScrollViewTxtExpression.scrollTo(binding.txtExpression.width, 0)
+            }
+
+        })
+
+        val viewTreeResult: ViewTreeObserver =
+            binding.horizontalScrollViewTxtResult.viewTreeObserver
+        viewTreeResult.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                binding.horizontalScrollViewTxtResult.viewTreeObserver.removeOnGlobalLayoutListener(
+                    this
+                )
+
+                binding.horizontalScrollViewTxtResult.scrollTo(binding.txtResult.width, 0)
+            }
+
+        })
 
     }
 
     private fun onOperatorClicked() {
 
-        binding.btnJam.setOnClickListener() {
-            appendText("+")
+        binding.btnSum.setOnClickListener() {
+            if (binding.txtExpression.text.isNotEmpty()) {
+                val myChar = binding.txtExpression.text.last()
+
+                if (myChar != '+' &&
+                    myChar != '-' &&
+                    myChar != '*' &&
+                    myChar != '/'
+                )
+                    appendText("+")
+            }
         }
 
-        binding.btnMenha.setOnClickListener() {
-            appendText("-")
+        binding.btnMinus.setOnClickListener() {
+            if (binding.txtExpression.text.isNotEmpty()) {
+
+                val myChar = binding.txtExpression.text.last()
+
+                if (myChar != '+' &&
+                    myChar != '-' &&
+                    myChar != '*' &&
+                    myChar != '/'
+                )
+                    appendText("-")
+            }
         }
 
-        binding.btnZarb.setOnClickListener() {
-            appendText("*")
+        binding.btnMultiplection.setOnClickListener() {
+            if (binding.txtExpression.text.isNotEmpty()) {
+
+                val myChar = binding.txtExpression.text.last()
+
+                if (myChar != '+' &&
+                    myChar != '-' &&
+                    myChar != '*' &&
+                    myChar != '/'
+                )
+                    appendText("*")
+            }
         }
 
-        binding.btnTaqsim.setOnClickListener() {
-            appendText("/")
+        binding.btnDivision.setOnClickListener() {
+            if (binding.txtExpression.text.isNotEmpty()) {
+
+                val myChar = binding.txtExpression.text.last()
+
+                if (myChar != '+' &&
+                    myChar != '-' &&
+                    myChar != '*' &&
+                    myChar != '/'
+                )
+                    appendText("/")
+            }
         }
 
-        binding.btnParantezBaz.setOnClickListener() {
+        binding.btnClosedParantez.setOnClickListener() {
             appendText("(")
         }
 
-        binding.btnParantezBaste.setOnClickListener() {
+        binding.btnOpenParantez.setOnClickListener() {
             appendText(")")
         }
 
@@ -102,6 +186,33 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnDelete.setOnClickListener {
 
+            val oldText = binding.txtExpression.text.toString()
+
+            if (oldText.isNotEmpty()) {
+                binding.txtExpression.text = oldText.substring(0, oldText.length - 1)
+            }
+        }
+
+        binding.btnEqual.setOnClickListener() {
+
+            try {
+                val expression = ExpressionBuilder(binding.txtExpression.text.toString()).build()
+                val result = expression.evaluate()
+
+                val longResult = result.toLong()
+
+                //135.0 == 135
+                if (result == longResult.toDouble()) {
+                    binding.txtResult.text = longResult.toString()
+                } else {
+                    binding.txtResult.text = result.toString()
+                }
+            } catch (ex: Exception) {
+                binding.txtResult.text = ""
+                binding.txtExpression.text = ""
+
+                Toast.makeText(this, "Error Occurred !", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
